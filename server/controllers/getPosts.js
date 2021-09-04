c9c676d3750e975fa55bc81ce6b400cb8e134a3a
Sean Lawlor <lawlorseanr@gmail.com>
2021-09-04 10:21:53 -0700

    Implement basic testing library

--- server/controllers/getPosts.js
+++ server/controllers/getPosts.js
@@ -1,5 +1,44 @@
 const Endpoint = require('../lib/Endpoint');
 
+const sortByAllowed = ['id', 'reads', 'likes', 'popularity'];
+const directionAllowed = ['asc', 'desc'];
+
+const checkSortBy = (sortBy) => {
+  if (sortBy === undefined) {
+    return sortByAllowed[0];
+  }
+  if (typeof sortBy === 'string' && sortByAllowed.indexOf(sortBy) >= 0) {
+    return sortBy;
+  }
+  return false;
+};
+
+const checkDirection = (direction) => {
+  if (direction === undefined) {
+    return directionAllowed[0];
+  }
+  if (typeof direction === 'string' && directionAllowed.indexOf(direction) >= 0) {
+    return direction;
+  }
+  return false;
+};
+
 module.exports = (req, res) => {
-  res.status(200).json('posts');
+  const params = req.params;
+
+  if (params.tags && params.tags.length > 0) {
+    const tags = params.tags;
+
+    const sortBy = checkSortBy(params.sortBy);
+    const direction = checkDirection(params.direction);
+    if (!sortBy) {
+      res.status(400).json({ error: 'sortBy parameter in invalid' });
+    } else if (!direction) {
+      res.status(400).json({ error: 'direction parameter is invalid' });
+    } else {
+      res.status(200).json({ success: true });
+    }
+  } else {
+    res.status(400).json({ error: 'Tags parameter is required' });
+  }
 };
---
+++ spec/controllers.test.js
@@ -0,0 +1,5 @@
+describe('Setup jest', () => {
+  test('jest works', () => {
+    expect(1).toEqual(1);
+  });
+});
