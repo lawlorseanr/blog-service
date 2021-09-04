41ecf436358fda10f6c5570bb41f22d7d47f4963
Sean Lawlor <lawlorseanr@gmail.com>
2021-09-04 09:48:30 -0700

    Implement GET /api/ping route to specifications

--- server/controllers/getPing.js
+++ server/controllers/getPing.js
@@ -1,3 +1,3 @@
 module.exports = (req, res) => {
-  res.status(200).json('ping');
+  res.status(200).json({ success: true });
 };
--- server/controllers/getPosts.js
+++ server/controllers/getPosts.js
@@ -1,3 +1,5 @@
+const Endpoint = require('../lib/Endpoint');
+
 module.exports = (req, res) => {
   res.status(200).json('posts');
 };
